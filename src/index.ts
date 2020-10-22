f93d78b22734e3b45f5b9e767c133027f4c6c611
syaripuddin <syaripuddin.digitalent>
2020-10-23 02:56:22 +0700

    initial  commit

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+// register root file untuk menggunakan sourcemap
+import 'source-map-support/register'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import bodyParser from 'body-parser'
+
+async function initApp() {
+  const app = express()
+
+  app.use(bodyParser.json())
+
+  app.post('/customer', function(req, res, next) {
+
+  })
+
+  app.get('/customer', function(req, res, next) {
+
+  })
+
+  app.get('/customer/:id', function(req, res, next) {
+
+  })
+
+  app.put('/customer', function(req, res, next) {
+
+  })
+
+  app.delete('/customer', function(req, res, next) {
+
+  })
+
+  app.use(function(err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
+    res.status(500).send({
+      success: false,
+      message: err.message
+    })
+  })
+
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`App listen on port ${ process.env.PORT || 8000 }`)
+  })
+}
+
+initApp()
